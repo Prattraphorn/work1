0fbf652ce5699f56cf66d8b7fdac5c3e30d32017
ice <ice.hiruntivakul@gmail.com>
2023-05-13 23:59:16 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int i = int.Parse(Console.ReadLine());
+		
+		while(i<0) {
+			Console.WriteLine("Invalid Pascal’s triangle row number.");
+			
+			i = int.Parse(Console.ReadLine());
+		}
+		for (int row = 0; row < i+1; row++)
+        {
+            int num = 1;
+            for (int col = 0; col <= row; col++)
+            {
+                Console.Write(num + " ");
+                num = num * (row - col) / (col + 1);
+            }
+            Console.WriteLine();
+        }
+	}
+}
